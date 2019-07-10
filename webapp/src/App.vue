938ab3808ef2b5c6310f5674ffeab94fdcbeadf3
Antonio Vivace <avivace4@gmail.com>
2019-07-11 00:49:49 +0200

    Unity -> JS interfacing proof of concept. Trigger Vue reactivity from inside the Unity (webgl deployed) gameInstance.

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,48 +1,70 @@
 <template>
   <div id="app">
-    <span class="brand">Physarum</span> <br>
+    <span class="brand">Physarum</span> <br />
     <span class="subtitle"><i>Slime mould simulation</i>, July 2019 </span>
     <div class="footer">
-      
-       <img src="https://upload.wikimedia.org/wikipedia/commons/1/19/Unity_Technologies_logo.svg" height="30px">&nbsp; <img src="https://upload.wikimedia.org/wikipedia/commons/2/25/WebGL_Logo.svg" height="32px">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/1/19/Unity_Technologies_logo.svg"
+        height="34px"
+      />&nbsp;
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/2/25/WebGL_Logo.svg"
+        height="38px"
+      />
     </div>
-   
+    <br /><br /><br />
+    {{ greetText }}
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app'
-}
+  name: "app",
+  data: function() {
+    return {
+      greetText: null
+    };
+  },
+  mounted() {},
+  methods: {
+    greet(text) {
+      this.greetText = text;
+    }
+  }
+};
 </script>
 
 <style>
 .brand {
-  font-family: 'Gentium Basic', serif;;
-  font-size: 5rem;
+  font-family: "Gentium Basic", serif;
+  font-size: 5.5rem;
   font-weight: 400;
   letter-spacing: -0.25rem;
-
+  background-color: #111;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  background-clip: text;
+  color: transparent;
+  text-shadow: rgba(255, 255, 255, 0.4) 0px 3px 3px;
 }
 
 .subtitle {
-  font-family: 'Gentium Basic', serif;
+  font-family: "Gentium Basic", serif;
   font-size: 1.5rem;
   font-weight: 400;
-
+  letter-spacing: -0.02rem;
 }
 #app {
-  font-family: 'Inter', Helvetica, Arial, sans-serif;
+  font-family: "Inter", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin-top: 5px;
+  font-size: 1.5rem;
 }
-.footer{
+.footer {
   position: fixed;
-
+  right: 0;
   bottom: 0;
-
 }
 </style>
--- webapp/src/main.js
+++ webapp/src/main.js
@@ -4,6 +4,10 @@ import "typeface-gentium-basic"
 
 Vue.config.productionTip = false
 
-new Vue({
+
+// Instance Vue and expose it to the global namespace, so we can 
+//  touch things from outside, accessing `vm.$children[0]`
+
+global.vm = new Vue({
   render: h => h(App),
 }).$mount('#app')
