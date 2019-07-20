ad46d183a189edab292f90f0f443ebd3e85031e4
Antonio Vivace <avivace4@gmail.com>
2019-07-20 23:53:01 +0200

    Exposed vuejs instance endpoint

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -6,7 +6,8 @@ Vue.config.productionTip = false
 
 
 // Instance Vue and expose it to the global namespace, so we can 
-//  touch things from outside, accessing `vm.$children[0]`
+//  touch things from outside, accessing `$vm[0]`
+//  Used for the Unity -> Vue comunication
 
 global.vm = new Vue({
   render: h => h(App),
