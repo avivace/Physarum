b1af9177a4c6b84ef6f5921b9afa6edd43d1cd01
Antonio Vivace <avivace4@gmail.com>
2019-07-24 13:14:22 +0200

    Barlow as sans serif font

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,11 +3,12 @@ import App from './App.vue'
 import "typeface-gentium-basic"
 import Vuetify from 'vuetify'
 import 'vue-feather-icons'
+import 'typeface-barlow'
+import 'vuetify/dist/vuetify.min.css' 
 
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 
-import 'vuetify/dist/vuetify.min.css' 
 
 // Instance Vue and expose it to the global namespace, so we can 
 //  touch things from outside, accessing `$vm[0]`
